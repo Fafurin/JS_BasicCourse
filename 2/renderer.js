2c229e7905a9d203deacc1a1e05e24b9eee6c262
Fedor <ffafurin@mail.ru>
2021-03-02 19:00:31 +0300

    Выполнил задание к 4 уроку.

---
+++ 2/renderer.js
@@ -0,0 +1,48 @@
+let renderer = {
+    // Сюда запишем все что надо отобразить.
+    map: "",
+
+    /**
+     * Отображает игру в консоли.
+     */
+    render() {
+        // Цикл перебирает все строки, которые надо отобразить.
+        for (let row = 0; row < config.rowsCount; row++) {
+            // В каждой строке отображаем для каждой колонки (x - клетка, o - игрок).
+            for (let col = 0; col < config.colsCount; col++) {
+                // Проверяем, если на данной позиции должен быть и игрок, отображаем игрока, если нет - клетку.
+				
+				if (player.y < 0) {
+					player.y = 0;
+				} 
+				if (player.x < 0) {
+					player.x = 0;
+				}
+				if (player.y >= config.rowsCount) {
+					player.y = config.colsCount - 1;
+				} 
+				if (player.x >= config.colsCount) {
+					player.x = config.colsCount -1;
+				} 				
+                if (player.y === row && player.x === col) {
+                    this.map += 'o ';
+                } else {
+                    this.map += 'x ';
+                }
+            }
+            // После того как отобразили всю строку делаем переход на следующую строку.
+            this.map += '\n';
+        }
+        
+        // Выводим все что надо отобразить в игре.
+        console.log(this.map);
+    },
+
+    clear() {
+        // Чистим консоль.
+        console.clear();
+        // Чистим карту.
+        this.map = "";
+    }
+};
+
